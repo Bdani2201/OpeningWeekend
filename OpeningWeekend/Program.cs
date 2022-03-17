3672cab999f47c3e056622ca0a072d59abc4d92f
Bdani2201 <91455909+Bdani2201@users.noreply.github.com>
2022-03-17 09:01:27 +0100

    1-2 feladat

--- OpeningWeekend/Program.cs
+++ OpeningWeekend/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,11 +6,32 @@ using System.Threading.Tasks;
 using System.IO;
 namespace OpeningWeekend
 {
+    class Film
+    {
+        public string EredetiCim;
+        public string MagyarCim;
+        public DateTime Bemutato;
+        public string Forgalmazo;
+        public double Bevetel;
+        public int Latogato;
+
+        public Film(string eredetiCim, string magyarCim, DateTime bemutato, string forgalmazo, double bevetel, int latogato)
+        {
+            EredetiCim = eredetiCim;
+            MagyarCim = magyarCim;
+            Bemutato = bemutato;
+            Forgalmazo = forgalmazo;
+            Bevetel = bevetel;
+            Latogato = latogato;
+        }
+    }
     class Program
     {
+        static List<Film> Filmek = new List<Film>();
         static void Main(string[] args)
         {
 
         }
     }
 }
+
